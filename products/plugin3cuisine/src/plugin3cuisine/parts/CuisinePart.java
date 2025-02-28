e42f63ea1ba76517efdf0d5b0e7422a8d1a3e0d1
karim maitig <karimmaitig98@gmail.com>
2025-02-28 15:27:54 +0100

    add common plugin

--- products/plugin3cuisine/src/plugin3cuisine/parts/CuisinePart.java
+++ products/plugin3cuisine/src/plugin3cuisine/parts/CuisinePart.java
@@ -8,8 +8,9 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 
-import plugin1cuisine.Plugin1Dialog;
-import plugin2cuisine.Plugin2Dialog;
+import plugin1common.Plugin1CommonDialog;
+import plugin1cuisine.Plugin1CuisineDialog;
+import plugin2cuisine.Plugin2CuisineDialog;
 
 public class CuisinePart {
 
@@ -19,22 +20,32 @@ public class CuisinePart {
 
         // Bouton 1 : Afficher Plugin 1
         Button btn1 = new Button(parent, SWT.PUSH);
-        btn1.setText("Afficher Plugin 1");
+        btn1.setText("Plugin 1 Cuisine");
         btn1.addListener(SWT.Selection, e -> openPlugin1Dialog(parent.getShell()));
 
         // Bouton 2 : Afficher Plugin 2
         Button btn2 = new Button(parent, SWT.PUSH);
-        btn2.setText("Afficher Plugin 2");
+        btn2.setText("Plugin 2 Cuisine");
         btn2.addListener(SWT.Selection, e -> openPlugin2Dialog(parent.getShell()));
+        
+        // Bouton 3 : Afficher Plugin 1 common 
+        Button btn3 = new Button(parent, SWT.PUSH);
+        btn3.setText("Plugin 1 common");
+        btn3.addListener(SWT.Selection, e -> openPlugin3Dialog(parent.getShell()));
     }
 
     private void openPlugin1Dialog(Shell shell) {
-        Plugin1Dialog dialog = new Plugin1Dialog(shell);
+        Plugin1CuisineDialog dialog = new Plugin1CuisineDialog(shell);
         dialog.open();
     }
 
     private void openPlugin2Dialog(Shell shell) {
-        Plugin2Dialog dialog = new Plugin2Dialog(shell);
+        Plugin2CuisineDialog dialog = new Plugin2CuisineDialog(shell);
+        dialog.open();
+    }
+    
+    private void openPlugin3Dialog(Shell shell) {
+        Plugin1CommonDialog dialog = new Plugin1CommonDialog(shell);
         dialog.open();
     }
 }
